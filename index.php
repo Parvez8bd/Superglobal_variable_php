6869cfd8b6f061de6e9d665b7a4e5c481c9b047f
Parvez8bd <parvez8bd@gmail.com>
2023-07-19 16:55:16 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Superg lobal</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4"></div>
+            <div class="col-lg-4 mt-3">
+                <h4>login page</h4>
+
+
+                <form action="index.php" method="post">
+                    <div>
+                        <label for="" class="form-label">phone</label>
+                        <input class="form-control" type="text" name="phone">
+                    </div>
+                    <div class="mt-3">
+                        
+                        <button class="btn btn-warning" type="reset">Reset</button>
+                        <button class="btn btn-primary" type="submit">save</button>
+                    </div>
+
+                </form>
+            </div>
+            <div class="col-lg-4"></div>
+        </div>
+    </div>
+
+
+
+
+
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $oldPhone = '123';
+        $phone = $_REQUEST['phone'];
+        if ($phone == $oldPhone) {
+            header('Location: welcome.php');
+            exit();
+        } else {
+            echo 'phone mele nai';
+        }
+    }
+    ?>
+
+</body>
+
+</html>
---
+++ welcome.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h4>welcome page</h4>
+<h1>Hi, welcome page</h1>
+
+</body>
+</html>
